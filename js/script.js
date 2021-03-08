0eb653e56b3f1d8833e6875c20c3e6f689e798c2
Dmitriy Latysh <pradovds@ukr.net>
2021-03-08 11:01:29 +0200

    Dropdawn added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,47 @@
-"use strict";
+window.addEventListener("DOMContentLoaded", () => {
+  //Dropdown menu
 
-//Dropdown menu
+  const dropdownParent = document.querySelector(".header__nav-dropdown"),
+    dropdownButton = document.querySelectorAll(".header__dropdown-btn"),
+    dropdownContent = document.querySelectorAll(".dropdown__content");
 
-const navDropdwn = document.querySelectorAll(".header__dropdown-element");
+  function hideDropdwnContent() {
+    dropdownContent.forEach((item) => {
+      item.classList.add("hide");
+      item.classList.remove("show", "fade");
+    });
 
+    dropdownButton.forEach((item) => {
+      item.classList.remove("dropdown-content-active");
+    });
+  }
+
+  function showDropdwnContent(i) {
+    dropdownContent[i].classList.add("show", "fade");
+    dropdownContent[i].classList.remove("hide");
+    dropdownButton[i].classList.add("dropdown-content-active");
+  }
+
+  hideDropdwnContent();
+
+  dropdownParent.addEventListener("click", (event) => {
+    const target = event.target;
+
+    if (target && target.classList.contains("header__dropdown-btn")) {
+      dropdownButton.forEach((item, i) => {
+        if (target == item) {
+          hideDropdwnContent();
+          showDropdwnContent(i);
+        }
+      });
+    }
+  });
+
+  window.addEventListener("click", (event) => {
+    const target = event.target;
+
+    if (!target.classList.contains("header__dropdown-btn")) {
+      hideDropdwnContent();
+    }
+  });
+});
