52fab7fd28a5d9d7dfbc684d595e34f633509cd1
Dmitriy Latysh <pradovds@ukr.net>
2021-03-09 12:11:35 +0200

    Slider added

--- js/script.js
+++ js/script.js
@@ -44,4 +44,44 @@ window.addEventListener("DOMContentLoaded", () => {
       hideDropdwnContent();
     }
   });
+
+  //Slider
+  const previous = document.querySelector('.previous'),
+        next = document.querySelector('.next');
+
+    previous.addEventListener('click', previousSlide);
+    next.addEventListener('click', nextSlide);
+
+  let slideIndex = 1;
+  showSlides(slideIndex);
+
+  function nextSlide() {
+    showSlides(slideIndex += 1);
+  }
+
+  function previousSlide() {
+    showSlides(slideIndex -= 1);
+  }
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    let slides = document.querySelectorAll('.blog__slide');
+
+    if(n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+
+    for (let slide of slides) {
+      slide.style.display = 'none';
+    }
+
+    slides[slideIndex - 1].style.display = 'block';
+  }
+  
 });
